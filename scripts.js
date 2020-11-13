e6c931ab76b29825b950b7b6dd38888ae810258c
pr0f3ss0rst3ph <stephonfraserofficial@gmail.com>
2020-11-13 19:13:30 -0400

    Patched Issues with Script. Added Comments.

--- scripts.js
+++ scripts.js
@@ -1,13 +1,29 @@
 var today = new Date();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-var hour = today.getHours();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds(); //Gets a full format of the date for provisioning purposes
+var hour = today.getHours(); //Get hours to determine the hour of the day
 
-if(hour<12){
-    document.getElementById('greeting').innerHTML="Good Morning! IT Support";
+if(hour<12){ //Hours below 12 are morning hours (This includes 0 and 1 which refers to midnight)
+    document.querySelector('#greeting').innerHTML="Good Morning! IT Support";
 }
-else if(hour>=12 && hour <= 18){
-    document.getElementById('greeting').innerHTML = "Good Afternoon! IT Support";
+else if(hour>=12 && hour <= 18){ //These hours are the afternoon period
+    document.querySelector('#greeting').innerHTML = "Good Afternoon! IT Support";
 }
 else{
-    document.getElementById('greeting').innerHTML = "Good Evening! IT Support";
+    document.querySelector('#greeting').innerHTML = "Good Evening! IT Support";
 }
+
+/*
+var today = new Date();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds(); //Gets a full format of the date for provisioning purposes
+var hour = today.getHours(); //Get hours to determine the hour of the day
+
+if(hour<12){ //Hours below 12 are morning hours (This includes 0 and 1 which refers to midnight)
+    console.log("Good Morning! IT Support");
+}
+else if(hour>=12 && hour <= 18){ //These hours are the afternoon period
+    console.log("Good Afternoon! IT Support");
+}
+else{
+    console.log("Good Evening! IT Support");
+}
+*/
